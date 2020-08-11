44c9de7542ad6bb9044b6f290fdaf45392b80738
jackson-kang <goodday1478@naver.com>
2020-08-11 19:52:10 +0900

    [bug-fixed] fix min_max_norm function

--- hparams.py
+++ hparams.py
@@ -1,6 +1,6 @@
 import os
 ### kss ###
-dataset = "kss"
+dataset = "kss_v_1.4"
 data_path = os.path.join("/home/minsu/dataset/", dataset)
 meta_name = "transcript.v.1.4.txt"	# "transcript.v.1.4.txt" or "transcript.v.1.3.txt" 
 textgrid_name = "TextGrid.zip"
@@ -44,7 +44,7 @@ max_seq_len = 1000
 # Quantization for F0 and energy
 ### kss ###
 f0_min = 71.0
-f0_max = 768.2
+f0_max = 792.8
 energy_min = 0.0
 energy_max = 283.72
 n_bins = 256
